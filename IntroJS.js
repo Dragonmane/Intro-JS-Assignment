f0557a5c2de3f04dc862c8d86e038e9dee91d298
Ted Ginn <tginn@mail.greenriver.edu>
2017-05-17 21:18:52 -0500

    Added files home and IntroJS to the folder

--- IntroJS.js
+++ IntroJS.js
@@ -1,1 +1,12 @@
-console.log("Introduction to JavaScript!")
+console.log("Introduction to JavaScript!");
+
+var number = 1;
+while (number <= 100){
+    console.log(number);
+    number = number + 1;
+}
+
+while (number <= 100){
+    console.log(number);
+    number = number + 3;
+}
